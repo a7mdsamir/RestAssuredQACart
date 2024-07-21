31541628edf4949973e50bbe07570dfb683e1b53
a7mdsamir <81165368+a7mdsamir@users.noreply.github.com>
2024-07-21 16:29:07 +0300

    Update TC01ResponseAssertion.java

--- src/test/java/com/qacart/TC01ResponseAssertion.java
+++ src/test/java/com/qacart/TC01ResponseAssertion.java
@@ -14,18 +14,19 @@ public class TC01ResponseAssertion {
            .when().get("users")
            .then()
                     .assertThat().statusCode(200)
-                    .assertThat().body( "[0].name" , equalTo("Tamara Bartell")) //v zt name of 1st item is tamara
-                    .assertThat().body( "name" , hasItem("Tamara Bartell"))  //v zt tamara is name of all names
-                    .assertThat().body( "name" , hasItems("Tamara Bartell" , "Dawn Reinger"))  //v zt tamara & Dawn are names of all names
+                    .assertThat().body( "[0].name" , equalTo("Evan Kling")) //v zt name of 1st item is Evan Kling OR
+                    //.assertThat().body( "[0].name" , is(equalTo("Evan Kling"))  )
+                    .assertThat().body( "name" , hasItem("Evan Kling"))  //v zt Evan is a name of all names
+                    .assertThat().body( "name" , hasItems("Evan Kling" , "Madiha"))  //v zt Evan & Madiha are names of all names
                     .assertThat().body("name" ,not(hasItem("samir")) ) //v zt samir is not a name of all names
                     //.assertThat().body( "name" , contains("Tamara Bartell" , "Dawn Reinger"))  //should enter all names arranged
                     .assertThat().body( "name" , not(empty()) ) //v zt key: name empty OR not
-                    .assertThat().body( "name" , hasSize(25) ) //v zt key: name has 25 value
-                    .assertThat().body( "name.size()" , equalTo(25) ) //v zt key: name has 25 value
+                    .assertThat().body( "name" , hasSize(50) ) //v zt key: name has 50 value
+                    .assertThat().body( "name.size()" , equalTo(50) ) //v zt key: name has 50 value
                     .assertThat().body( "avatar" , everyItem(startsWith("http")) ) //v zt all key: avatar start wiz http
                     .assertThat().body( "[0]" , hasKey("id") ) //v zt the 1st item has key = id
-                    .assertThat().body( "[0]" , hasValue("2023-08-09T05:24:39.630Z") ) //v zt the 1st item has value = "2023-08-09T05:24:39.630Z"
-                    .assertThat().body( "[0]" , hasEntry("createdAt" , "2023-08-09T05:24:39.630Z") ) //v zt the 1st item has key: "createdAt" and value: "2023-08-09T05:24:39.630Z"
+                    .assertThat().body( "[0]" , hasValue("2024-07-20T13:45:17.091Z") ) //v zt the 1st item has value = "2023-08-09T05:24:39.630Z"
+                    .assertThat().body( "[0]" , hasEntry("createdAt" , "2024-07-20T13:45:17.091Z") ) //v zt the 1st item has key: "createdAt" and value: "2023-08-09T05:24:39.630Z"
 
                     .log().all();
 
